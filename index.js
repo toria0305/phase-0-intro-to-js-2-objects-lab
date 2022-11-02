f99962984262e95b011ca6870282525895ba2dd3
Victoria Maggay <toria0305@gmail.com>
2022-11-02 00:09:50 -0400

    commit tree 50bf97de373ab2c26b23a7ddffdd6cf234d03244

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+var name = "employee1";
+var streetAddress = "location1";
+
+var employee = {name: streetAddress };
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = {...obj};
+
+    newObj[key] = value;
+
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue( employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key) {
+    let ploy2 = {...employee};
+    delete ploy2[key];
+    return ploy2;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
